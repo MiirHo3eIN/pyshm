5e142d9043691c9eb9b461fac2aa5a3fdd2d88a4
MiirHo3eIN <amirhmoallemi@gmail.com>
2022-06-03 09:43:39 +0200

    setup & license added

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup 
+
+
+setup(
+
+	name = 'pyshm', 
+	version = '0.0.1',
+	description = 'Python frequent functions for SHM application',
+	py_modules = ["frequency", "time"], 
+	package_dir = {'': 'src'}
+
+
+
+
+	)
