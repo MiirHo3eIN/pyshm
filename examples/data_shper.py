b99cee85b9ce2a06898fb250b90c1b1b4545865d
MiirHo3eIN <amirhmoallemi@gmail.com>
2024-06-02 12:58:38 +0200

    test of Mean Centering added

--- examples/data_shper.py
+++ examples/data_shper.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import seaborn as sns
-from pyshm.dataShaper import shaper
+from pyshm.dataShaper import shaper, MeanCentering
 
 def tester(): 
     N = 10_000
@@ -38,6 +38,17 @@ def tester():
         for i, clr in zip(range(5), colors): 
             plt.title("No Overlapp for a sequence of 1 second stride")
             plt.plot(t_shaped[i, :] ,x_shaped[i, :], color = clr, label = f'Shaped Signal {i}', linestyle = '--')
+            plt.legend()
+    
+    MeanCenterer = MeanCentering(axis = 1)
+    x_mean_centered = MeanCenterer(x_shaped)
+
+    with sns.plotting_context("poster"):
+        plt.figure(figsize= (16, 4))
+        plt.plot(t[:500], x[:500], color = 'green', label = 'Original Signal')
+        for i, clr in zip(range(5), colors): 
+            plt.title("Mean Centered Data")
+            plt.plot(t_shaped[i, :] ,x_mean_centered[i, :], color = clr, label = f'Shaped Signal {i}', linestyle = '--')
             plt.legend()
     
     
